8e3ba0fb22eac53aea63175bfa6ee54961690eb6
AZO234 <sylph23k@gmail.com>
2019-08-21 01:38:48 +0900

    fix

--- 1_WPS/setup.bat
+++ 1_WPS/setup.bat
@@ -3,5 +3,5 @@ powercfg.exe -x -standby-timeout-ac 0
 powercfg.exe -x -hibernate-timeout-ac 0
 
 cd C:\RAcross_windows\1_WPS
-powershell -WorkingDirectory ~ -ExecutionPolicy RemoteSigned -File .\RAcross.ps1
+powershell -ExecutionPolicy RemoteSigned -File .\RAcross.ps1
 
